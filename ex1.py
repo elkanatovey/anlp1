3650238a2e6dc24ed3b93c6ac9285cb267b22b74
elkana <elkanatovey@gmail.com>
2023-05-23 16:10:45 +0300

    load data done

--- ex1.py
+++ ex1.py
@@ -2,19 +2,26 @@ from dataclasses import dataclass, field
 from transformers import HfArgumentParser, TrainingArguments
 from datasets import load_dataset
 
+
 @dataclass
 class runParams:
-    num_seeds: int = field(metadata={"help": "number of seeds to train with"})
-    num_train_samples: int
-    num_validation_samples: int
-    num_samples_to_predict: int
+    num_seeds: int = field(default=0, metadata={"help": "number of seeds to train with"})
+    num_train_samples: int = field(default=-1, metadata={"help": "number of train examples to use"})
+    num_validation_samples: int = field(default=-1, metadata={"help": "number of validation examples to use"})
+    num_samples_to_predict: int = field(default=-1, metadata={"help": "number of samples to predict a sentiment for"})
 
 def main():
-    parser = HfArgumentParser((runParams, TrainingArguments))
-    my_params, training_args = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser((runParams))
+    my_params = parser.parse_args_into_dataclasses()[0]
 
+    # load data
     dataset = load_dataset("sst2")
-
+    train_data = dataset['train'] if my_params.num_train_samples == -1 \
+        else dataset['train'].select(range(my_params.num_train_samples))
+    validation_data = dataset['validation'] if my_params.num_validation_samples == -1\
+        else dataset['validation'].select(range(my_params.num_validation_samples))
+    test_data = dataset['test'] if my_params.num_samples_to_predict == -1 \
+        else dataset['test'].select(range(my_params.num_samples_to_predict))
 
 if __name__ == '__main__':
     main()
